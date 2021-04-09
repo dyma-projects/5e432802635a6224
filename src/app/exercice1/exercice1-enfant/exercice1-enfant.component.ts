c475784ee3e2c0f4a65824c45e02f84ecc568d8d
BILLAULT Fabien <fabien.billault@nr-communication.fr>
2021-04-09 11:58:27 +0200

    Projet 3

--- src/app/exercice1/exercice1-enfant/exercice1-enfant.component.ts
+++ src/app/exercice1/exercice1-enfant/exercice1-enfant.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
 
 @Component({
-  selector: 'app-exercice1-enfant',
-  templateUrl: './exercice1-enfant.component.html',
-  styleUrls: ['./exercice1-enfant.component.css']
+  selector: "app-exercice1-enfant",
+  templateUrl: "./exercice1-enfant.component.html",
+  styleUrls: ["./exercice1-enfant.component.css"],
 })
 export class Exercice1EnfantComponent implements OnInit {
+  @Input() counterValue: number;
+  @Output() counterValueEvent = new EventEmitter();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  public increment() {
+    this.counterValue++;
+    this.counterValueEvent.emit({
+      value: this.counterValue,
+    });
   }
 
+  public decrement() {
+    this.counterValue--;
+    this.counterValueEvent.emit({
+      value: this.counterValue,
+    });
+  }
 }
